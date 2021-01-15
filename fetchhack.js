a4cec1056d11a7ffe82a58953225a9e366c46737
GlennCampbell <glenn@gcampbell.co.uk>
2021-01-15 10:22:57 +0000

    Commented Plan Started.

--- fetchhack.js
+++ fetchhack.js
@@ -5,3 +5,31 @@ async function sendFetch(){
 }
 sendFetch();
 
+// Daily Quiz web page.
+
+// get a list?? of questions from api and store in variable
+
+// or just get one at a time.
+// https://opentdb.com/api.php?amount=1&category=11&difficulty=easy&type=boolean
+// ask a true / false quiz question
+
+
+// get answer from user.
+// ?? lose a star if not answered in time
+
+
+// score answer on star score board.
+
+
+
+// keep asking question until 5 has been scored
+
+
+// don't ask duplicate questions
+
+
+// when five stars, change difficulty
+
+
+// list answered questions below with ?? fade out maybe
+
